303b880f93f560eab8b33f9fb274f1d3accfbe6d
Scott Wachal <scott.wachal@gmail.com>
2021-09-27 10:28:19 -0500

    created seeds.rb to populate our DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+Movie.delete_all
+ProductionCompany.delete_all
+
+filename = Rails.root.join("db/top_movies.csv")
+
+puts "Loading Movies from the CSV file: #{filename}"
+
+csv_data = File.read(filename)
+movies = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+movies.each do |m|
+  # puts m["original_title"]
+  production_company = ProductionCompany.find_or_create_by(name: m["production_company"])
+
+  if production_company && production_company.valid?
+    movie = production_company.movies.create(
+      title:        m["original_title"],
+      year:         m["year"],
+      duration:     m["duration"],
+      description:  m["description"],
+      average_vote: m["avg_vote"]
+    )
+
+    # movie && movie.valid?
+    puts "Invalid movie #{m['original_title']}" unless movie&.valid?
+  else
+    puts "Invalid Production Company: #{m['production_company']} for movie: #{m['original_title']}"
+  end
+end
+
+puts "Created #{ProductionCompany.count} Production Companies"
+puts "Created #{Movie.count} movies."
